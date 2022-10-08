1d1156b129bf803eb73226a1e9376f588fc8ea3e
DSR! <xchwarze@gmail.com>
2022-10-08 17:29:02 -0300

    Sync version with main repo

---
+++ scripts/IDR.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+echo Fix IDR folder structure
+cd "updates\master\IDR-master"
+del *.cpp
+del *.dfm
+del *.h
+del *.rc
+del *.res
+del *.manifest
+move bin\Idr.exe Idr.exe
---
+++ scripts/MultiMon.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+echo Make MultiMon Portable
+cd "updates/MultiMonHome"
+"../../bin/innounp.exe" -e "MultiMonHome.exe"
+del "install_script.iss"
+del "MultiMonHome.exe"
